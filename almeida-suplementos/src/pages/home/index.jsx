d252c717eb96b76bedc2eafb40bb31183c521a56
Lucas Gabriel Garcia <lucasgabrielg287@gmail.com>
2024-10-13 12:38:53 -0300

    T002: feat - Estruturação do HTML da página Home

--- almeida-suplementos/src/pages/home/index.jsx
+++ almeida-suplementos/src/pages/home/index.jsx
@@ -5,9 +5,67 @@ function Home() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <div>
+      <header>
+        <img src="" alt="" />
+        <h1>SUA FONTE DE <span>SUPLEMENTOS</span> PREMIUM</h1>
+      </header>
+      <div>
+        COMPONENTE NAVBAR
+      </div>
+      <main>
+        <hr />
+        <div>
+          <h2>
+            PRODUTOS EM <span>PROMOÇÃO</span>
+          </h2>
+          COMPONENTES DOS CARDS DE PRODUTOS
+        </div>
+        <hr />
+        <div>
+          <h2>
+            PRODUTOS MAIS <span>BUSCADOS</span>
+          </h2>
+          COMPONENTES DOS CARDS DE PRODUTOS
+        </div>
+        <div>
+          COMPONENTES DE CARDS BUTTON
+        </div>
+      </main>
+      <section>
+        <h2>
+          FAÇA SUA COMPRA PELO <span>WHATSAPP</span>
+        </h2>
+        COMPONENTE BUTTON WHATSAPP
+        <p>Converse diretamente com um vendedor pelo WhatsApp clicando no botão acima!</p>
+      </section>
+      <footer>
+        <img src="" alt="" />
+        <div>
+          <p>NOS SIGA NO <span>INSTAGRAM</span> PARA VER TODAS AS NOVIDADES</p>
+          <button>
+            <img src="" alt="" />
+          </button>
+        </div>
+        <p>
+          ENTRE EM CONTATO DIRETAMENTE COM UM VENDEDOR
+        </p>
+        COMPONENTE BUTTON WHATSAPP
+        <hr />
+        <div>
+          <p>Desenvolvido por:</p>
+          <p>Lucas Garcia</p>
+          <button>LINKEDIN</button>
+          <button>GITHUB</button>
+          <button>INSTAGRAM</button>
+        </div>
+        <hr />
+        <p>
+          EXEMPLO DE NOME LTDA - Rua Fulano de tal, 100 - Ribeirão Branco - SP CEP: 18.430-000 - CNPJ: 00.000.000/0000-00 © 
+          2024 Todos os direitos reservados.
+        </p>
+      </footer>
+    </div>
   )
 }
 
