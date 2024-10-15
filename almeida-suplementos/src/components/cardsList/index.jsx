0687e8826b405f7953abfb38e88eb97bc3a590fa
Lucas Gabriel Garcia <lucasgabrielg287@gmail.com>
2024-10-15 12:03:27 -0300

    T004: chore - Criação e estilização inicial do componente CardsList

---
+++ almeida-suplementos/src/components/cardsList/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+import Card from '../card'
+
+function CardsList () {
+    return (
+        <div className='list'>
+            <Card />
+        </div>
+    )
+}
+
+export default CardsList
