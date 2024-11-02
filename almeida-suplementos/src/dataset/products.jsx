50b62843fb3e22ab02b1cc4086a144f849f0761d
Lucas Gabriel Garcia <lucasgabrielg287@gmail.com>
2024-11-01 22:44:14 -0300

    T008: feat - Criação do arquivo dataset de produtos do site

---
+++ almeida-suplementos/src/dataset/products.jsx
@@ -0,0 +1,46 @@
+const PRODUCTS = [
+    {
+        id: 2511,
+        name: "100% Whey 900G - Max Titanium",
+        desc: "Whey protein Growth fornece proteínas para quem deseja hipertrofia e definição muscular. Ideal porque é um suplemento de alto valor biológico com grande concentração de proteínas e aminoácidos essenciais é também rico em Glutamina, BCAA (incluindo Leucina).",
+        price: "R$150,00",
+        image: "https://firebasestorage.googleapis.com/v0/b/almeida-suplementos-db.appspot.com/o/images-products%2Fwhey-max.jpeg?alt=media&token=56e54240-94eb-4b7f-9bbb-c293dbe68ce2",
+    },
+    {
+        id: 4049,
+        name: "Creatina 300G - Max Titanium",
+        desc: "A creatina auxilia no aumento do desempenho físico durante exercícios repetidos de curta duração e alta intensidade. Formada a partir da associação de três aminoácidos de alto valor biológico (arginina, glicina e metionina), a creatina está presente naturalmente em nosso organismo, sendo que cerca de 95% do seu conteúdo é armazenado no músculo esquelético. Recomenda-se a ingestão desta substância juntamente com um carboidrato de alto índice glicêmico para facilitar o seu transporte para dentro das células musculares.",
+        price: "R$125,00",
+        image: "https://firebasestorage.googleapis.com/v0/b/almeida-suplementos-db.appspot.com/o/images-products%2Fcreatina-max.jpeg?alt=media&token=f19fdc83-fc0c-4cac-abe9-b6709d25e5af",
+    },
+    {
+        id: 9557,
+        name: "Pasta de amendoim Avelã - Dr. Peanut",
+        desc: "Viva uma experiência única com a Pasta de Avelã Dr. Peanut, onde cada momento de consumo se transforma em uma celebração de  e estilo. Perfeita para ser eada sozinha, como recheio de doces ou até mesmo em pratos sofisticados que pedem um toque especial.",
+        price: "R$60,00",
+        image: "https://firebasestorage.googleapis.com/v0/b/almeida-suplementos-db.appspot.com/o/images-products%2Fpeanut-avela.jpeg?alt=media&token=e3c890f4-e583-443e-8800-45ff6c48f570",
+    },
+    {
+        id: 5242,
+        name: "Pasta de amendoim Beijinho - Dr. Peanut",
+        desc: "Viva uma experiência única com a Pasta de Avelã Dr. Peanut, onde cada momento de consumo se transforma em uma celebração de  e estilo. Perfeita para ser eada sozinha, como recheio de doces ou até mesmo em pratos sofisticados que pedem um toque especial.",
+        price: "R$60,00",
+        image: "https://firebasestorage.googleapis.com/v0/b/almeida-suplementos-db.appspot.com/o/images-products%2Fpeanut-beijinho.jpeg?alt=media&token=5f6b3744-6dea-48fc-961f-4b6324b9f9ed",
+    },
+    {
+        id: 2753,
+        name: "Pasta de amendoim Cookies & Cream - Dr. Peanut",
+        desc: "Viva uma experiência única com a Pasta de Avelã Dr. Peanut, onde cada momento de consumo se transforma em uma celebração de  e estilo. Perfeita para ser eada sozinha, como recheio de doces ou até mesmo em pratos sofisticados que pedem um toque especial.",
+        price: "R$60,00",
+        image: "https://firebasestorage.googleapis.com/v0/b/almeida-suplementos-db.appspot.com/o/images-products%2Fpeanut-cookies.jpeg?alt=media&token=c3259d20-a717-435a-8e11-5b751b26d55e",
+    },
+    {
+        id: 8562,
+        name: "Pasta de amendoim Paçoca Crunchy - Dr. Peanut",
+        desc: "Viva uma experiência única com a Pasta de Avelã Dr. Peanut, onde cada momento de consumo se transforma em uma celebração de  e estilo. Perfeita para ser eada sozinha, como recheio de doces ou até mesmo em pratos sofisticados que pedem um toque especial.",
+        price: "R$60,00",
+        image: "https://firebasestorage.googleapis.com/v0/b/almeida-suplementos-db.appspot.com/o/images-products%2Fpeanut-pacoca.jpeg?alt=media&token=c09fbf52-d7b8-4732-b59e-def01c46b2ba",
+    }
+]
+
+export default PRODUCTS
