d666caaf9187aff97f85b520e87276361b182439
Lucas Gabriel Garcia <lucasgabrielg287@gmail.com>
2024-12-09 21:39:40 -0300

    T009: build - Criação e estilização geral da página Product

--- almeida-suplementos/src/pages/product/index.jsx
+++ almeida-suplementos/src/pages/product/index.jsx
@@ -1,9 +1,47 @@
-import './style.css'
+import { useParams } from 'react-router-dom'
+import { useEffect } from 'react';
 
-function Product () {
+import './style.css'
+import PRODUCTS from '../../dataset/products';
+import WhatsButton from '../../components/whatsButton';
+import Background from '../../assets/fundo-header.svg'
+import CardsList from '../../components/cardsList';
+
+function Product() {
+
+
+    const { id } = useParams();
+    const product = PRODUCTS.find((item) => item.id === id);
+
+
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    }, [id]);
+
     return (
         <div>
-            
+            <div className='productDiv'>
+                <img src={Background} className='backgroundImg' />
+                <img src={product.image} className='productImage' />
+                <div className='productInfos'>
+                    <h2 className='nameProd'>{product.name}</h2>
+                    <p className='descProd'>{product.desc}</p>
+                    <p className='priceProd'>{product.price}</p>
+                    <WhatsButton/>
+                </div>
+            </div>
+            <div className='newProducts'>
+                <h2>MAIS <span className='txtCor'>PRODUTOS</span></h2>
+                <CardsList/>
+            </div>
+            <div className='divWhats'>
+                <h2 className='txtWhats'>FAÇA SUA COMPRA PELO <span className='txtGreen'>WHATSAPP</span></h2>
+                <WhatsButton/>
+                <p className='txtAction'>Converse diretamente com um vendedor pelo <br/> WhatsApp clicando no botão acima!</p>
+            </div>
         </div>
     )
 }
