eb553bd9188677ed2a082ee281b1581b8d69aad4
Lucas Gabriel Garcia <lucasgabrielg287@gmail.com>
2025-01-04 13:52:45 -0300

    T010: chore - ajuste na estrutura e estilização do componente Navbar

--- almeida-suplementos/src/components/navbar/index.jsx
+++ almeida-suplementos/src/components/navbar/index.jsx
@@ -18,14 +18,15 @@ const location = useLocation();
                 <button type='text' className='btnText'>
                     <a className='linkBtn' href="#ofertas">OFERTAS</a>
                 </button>
+                </>
+            )}
                 <button type='text' className='btnText'>
                     <a className='linkBtn' href="#produtos">PRODUTOS</a>
                 </button>
                 <span className='line'></span>
-                </>
-            )}
+                
                 <button type='text' className='btnContato'>
-                    <a className='linkBtn linkContact' target='_blank' href="https://api.whatsapp.com/send?phone=5515997357627&text=Olá,%20gostaria%20de%20saber%20mais%20sobre%20os%20produtos%20da%20loja%20Almeida%20Suplementos">CONTATO</a>
+                    <a className='linkContact' target='_blank' href="https://api.whatsapp.com/send?phone=5515997357627&text=Olá,%20gostaria%20de%20saber%20mais%20sobre%20os%20produtos%20da%20loja%20Almeida%20Suplementos">CONTATO</a>
                 </button>
             </div>
             
