7b76b04f7de1fa1ad771bd320ac8d44154b914f8
Lucas Gabriel Garcia <lucasgabrielg287@gmail.com>
2025-04-04 18:50:34 -0300

    V2T002: fix - Correção no modulo do CSS do componente Footer

--- almeida-suplementos/src/components/footer/index.jsx
+++ almeida-suplementos/src/components/footer/index.jsx
@@ -1,4 +1,4 @@
-import './style.css'
+import styles from './style.module.css'
 import WhiteLogo from '../../assets/logo-branca.svg'
 import InstaLogo from '../../assets/insta-logo.svg'
 import LinkedinLogo from '../../assets/linkedin-logo.svg'
@@ -7,47 +7,47 @@ import WhatsButton from '../whatsButton'
 
 function Footer() {
     return (
-        <div className='divFooter'>
-            <img src={WhiteLogo} alt="" className='imgLogoLoja' />
-            <div className='socialNetDiv'>
-                <p className='footerTxt'>NOS SIGA NO <span className='newColorTxt'>INSTAGRAM</span> PARA VER TODAS AS NOVIDADES</p>
-                <button className='socialBtn'>
+        <div className={styles.divFooter}>
+            <img src={WhiteLogo} alt="Almeida Suplementos" className={styles.imgLogoLoja} />
+            <div className={styles.socialNetDiv}>
+                <p className={styles.footerTxt}>NOS SIGA NO <span className={styles.txtCor}>INSTAGRAM</span> PARA VER TODAS AS NOVIDADES</p>
+                <button className={styles.socialBtn}>
                     <a href="https://www.instagram.com/almeida_suplementosrb/" target='_blank'>
-                        <img src={InstaLogo} className='socialImg' />
+                        <img src={InstaLogo} alt='Almeida Suplementos Instagram' className={styles.socialImg} />
                     </a>
                 </button>
             </div>
-            <div className='whatsDivButton'>
-                <p className='footerTxt'>
+            <div className={styles.whatsDivButton}>
+                <p className={styles.footerTxt}>
                     ENTRE EM CONTATO DIRETAMENTE COM UM VENDEDOR
                 </p>
                 <WhatsButton />
             </div>
-            <hr className='line1' />
-            <div className='footerTxt'>
+            <hr className={styles.line1} />
+            <div className={styles.footerTxt}>
                 <p>Desenvolvido por:</p>
-                <p className='devName'>Lucas Garcia</p>
-                <div className='footerDevBtn'>
-                    <button className='socialBtn'>
+                    <p className={styles.devName}>Lucas Garcia</p>
+                <div className={styles.footerDevBtn}>
+                    <button className={styles.socialBtn}>
                         <a href="https://www.linkedin.com/in/garcialucasga/" target='_blank' >
-                            <img src={LinkedinLogo} alt="LinkedIn" className='socialImg' />
+                            <img src={LinkedinLogo} alt="LinkedIn" className={styles.socialImg} />
                         </a>
                     </button>
-                    <button className='socialBtn'>
+                    <button className={styles.socialBtn}>
                         <a href="https://github.com/OlucasGarcia" target='_blank' >
-                            <img src={GitHubLogo} alt="GitHub" className='socialImg' />
+                            <img src={GitHubLogo} alt="GitHub" className={styles.socialImg} />
                         </a>
                     </button>
-                    <button className='socialBtn'>
+                    <button className={styles.socialBtn}>
                         <a href="https://www.instagram.com/oluucasgarcia/" target='_blank' >
-                            <img src={InstaLogo} alt="Instragram" className='socialImg' />
+                            <img src={InstaLogo} alt="Instragram" className={styles.socialImg} />
                         </a>
                     </button>
                 </div>
 
             </div>
-            <hr className='line2' />
-            <p className='docTxt'>
+            <hr className={styles.line2} />
+            <p className={styles.docTxt}>
                 56.998.240 KAIQUE GABRIEL DE OLIVEIRA ALMEIDA - RUA JOSE GOMES RODRIGUES, 43 - Ribeirão Branco - SP CEP: 18.430-000 - CNPJ: 56.998.240/0001-81 ©
                 <br></br>2024 Todos os direitos reservados.
             </p>
