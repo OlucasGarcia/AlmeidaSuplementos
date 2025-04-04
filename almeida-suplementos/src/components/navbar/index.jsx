0d5f570d7bebb36045922a5af2053b75bd0a4ede
Lucas Gabriel Garcia <lucasgabrielg287@gmail.com>
2025-04-04 18:49:26 -0300

    V2T002: fix - Correção no modulo do CSS do componente Navbar

--- almeida-suplementos/src/components/navbar/index.jsx
+++ almeida-suplementos/src/components/navbar/index.jsx
@@ -1,4 +1,4 @@
-import './style.css'
+import styles from './style.module.css'
 import { Link, useLocation } from 'react-router-dom'
 
 //import de imagens e icons usados no componente
@@ -10,35 +10,35 @@ function Navbar() {
     const location = useLocation();
 
     return (
-        <div className='navbar'>
+        <div className={styles.navbar}>
             <Link to='/' style={{ display: 'flex', alignItems: 'center' }}>
-                <img src={Logo} alt="Almeida Suplementos" className='logo' />
+                <img src={Logo} alt="Almeida Suplementos" className={styles.logo} />
             </Link>
 
-            <ul className='buttons'>
+            <ul className={styles.buttons}>
 
                 {/* useLocation usado para separar conteúdos que aparecerão em apenas algumas páginas definitas pelo pathname */}
                 {location.pathname === "/" && (
                     <>
-                        <li className='btnText'>
-                            <a className='linkBtn' href="#ofertas">OFERTAS</a>
+                        <li className={styles.btnText}>
+                            <a className={styles.linkBtn} href="#ofertas">OFERTAS</a>
                         </li>
                     </>
                 )}
-                <li className='btnText'>
-                    <a className='linkBtn' href="#produtos">PRODUTOS</a>
+                <li className={styles.btnText}>
+                    <a className={styles.linkBtn} href="#produtos">PRODUTOS</a>
                 </li>
-                <span className='line'></span>
+                <span className={styles.line}></span>
 
-                <li className='btnContact'>
-                    <a className='linkContact' target='_blank' href="https://api.whatsapp.com/send?phone=5515997357627&text=Olá,%20gostaria%20de%20saber%20mais%20sobre%20os%20produtos%20da%20loja%20Almeida%20Suplementos">CONTATO</a>
+                <li className={styles.btnContact}>
+                    <a className={styles.linkContact} target='_blank' href="https://api.whatsapp.com/send?phone=5515997357627&text=Olá,%20gostaria%20de%20saber%20mais%20sobre%20os%20produtos%20da%20loja%20Almeida%20Suplementos">CONTATO</a>
                 </li>
             </ul>
 
             {/* div dropdown aparece somente com a tela em formato mobile */}
-            <div className="dropdown">
-                <img src={MenuIcon} className="menuBtn" />
-                <div className="dropdown-content">
+            <div className={styles.dropdown}>
+                <img src={MenuIcon} className={styles.menuBtn} />
+                <div className={styles.dropdownContent}>
                     {location.pathname === "/" && (
                         <a href="#ofertas">OFERTAS</a>
                     )}
