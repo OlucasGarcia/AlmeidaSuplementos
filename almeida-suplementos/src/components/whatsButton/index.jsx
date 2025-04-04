c3cdaddbaa68163eb70591d613cbc02e498011f8
Lucas Gabriel Garcia <lucasgabrielg287@gmail.com>
2025-04-04 18:50:56 -0300

    V2T002: fix - Correção no modulo do CSS do componente WhatsButton

--- almeida-suplementos/src/components/whatsButton/index.jsx
+++ almeida-suplementos/src/components/whatsButton/index.jsx
@@ -1,12 +1,12 @@
-import './style.css'
+import styles from './style.module.css'
 import WhatsappLogo from '../../assets/whatsapp-logo.svg'
 
 function WhatsButton () {
     return (
-        <div className='btnWhats'>
-            <a className='whatsTxt' target='_blank' href="https://api.whatsapp.com/send?phone=5515997357627&text=Olá,%20gostaria%20de%20saber%20mais%20sobre%20os%20produtos%20da%20loja%20Almeida%20Suplementos">
+        <div className={styles.btnWhats}>
+            <a className={styles.whatsTxt} target='_blank' href="https://api.whatsapp.com/send?phone=5515997357627&text=Olá,%20gostaria%20de%20saber%20mais%20sobre%20os%20produtos%20da%20loja%20Almeida%20Suplementos">
             <p>CONVERSAR <br></br>COM VENDEDOR</p>
-            <img src={WhatsappLogo} alt="Logo WhatsApp" className='imgLogo'/>
+            <img src={WhatsappLogo} alt="WhatsApp" className={styles.imgLogo} />
             </a>
         </div>
     )
